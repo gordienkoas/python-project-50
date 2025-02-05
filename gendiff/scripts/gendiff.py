d659d005fc56f6b8c1f91a2fba2c3c80efcf5d69
Andrey <gordienkoas@.bk.ru>
2025-02-05 17:08:30 +0300

    make lint (done)

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,7 +1,7 @@
-#import argparse
+# import argparse
 
 
-#def main():
+# def main():
     # parser = argparse.ArgumentParser(
     #     description='Compares two configuration files and shows a difference.'
     # )
@@ -17,9 +17,14 @@ from generate_diff import generate_diff
 file_path1 = 'file1.json'
 file_path2 = 'file2.json'
 
+
 def main():
-    diff = generate_diff('/home/user/PycharmProjects/python-project-50/gendiff/file1.json', '/home/user/PycharmProjects/python-project-50/gendiff/file2.json')
+    diff = generate_diff(
+        '/home/user/PycharmProjects/python-project-50/gendiff/file1.json',
+        '/home/user/PycharmProjects/python-project-50/gendiff/file2.json'
+    )
     print(diff)
+
 
 if __name__ == '__main__':
     main()

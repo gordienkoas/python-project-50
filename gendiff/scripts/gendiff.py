4b6faeee59c6a62e1a2cd911ff9a7466a4a43b40
Andrey <gordienkoas@.bk.ru>
2025-02-12 12:07:25 +0300

    step 5 done

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,16 +1,19 @@
+#!/usr/bin/env python3
+import argparse
 
-
-from generate_diff import generate_diff
+from gendiff.gendiff import generate_diff
 
 
 def main():
-    diff = generate_diff(
-        '/home/user/PycharmProjects/python-project-50/gendiff/file1.json',
-        '/home/user/PycharmProjects/python-project-50/gendiff/file2.json'
-    )
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
+    args = parser.parse_args()
+    diff = generate_diff(args.first_file, args.second_file)
     print(diff)
 
 
 if __name__ == '__main__':
     main()
-
